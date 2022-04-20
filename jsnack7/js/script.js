7bdaf06b22987a0062773369eb3227dcbab9b35f
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-20 13:59:39 +0200

    jsnack3 added with for

--- jsnack7/js/script.js
+++ jsnack7/js/script.js
@@ -1,1 +1,14 @@
 // jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
+
+// chiedi all utente un numero
+let userNumber = parseInt(prompt('Inserisci un numero'));
+// console.log(userNumber);
+
+//visualizza i numeri che precedono l'userNumber e fai il cubo
+for (let i = userNumber; i > 0; i--) {  
+    let power = i**userNumber;
+    console.log(i,`elevato alla ${userNumber}=`, power);
+}
+
+
+
