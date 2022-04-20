5b4e1c02e402194d2ac6d4d6c292af21730e3d68
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-20 13:36:11 +0200

    snack 2 completato

--- jsnack6/js/script.js
+++ jsnack6/js/script.js
@@ -1,2 +1,12 @@
 // jsnack2. Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e una
 // // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+for (let i = 0; i < 3; i++) {
+    let names = ['Alex', 'Eviatar', 'Lewis', 'Sana'];
+    let surnames = ['Rossi', 'White', 'Joel', 'Parrow'];
+
+    const randomIndexName = Math.floor(Math.random() * names.length);
+
+    const randomIndexSurname = Math.floor(Math.random() * surnames.length);
+
+    console.log(names[randomIndexName], surnames[randomIndexSurname]);
+}
