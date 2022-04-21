eb5b9ce27e0394558f1bd9b9a5ea7513cab98bc2
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-21 10:02:20 +0200

    code with do-while

--- jsnack7/js/script.js
+++ jsnack7/js/script.js
@@ -1,14 +1,29 @@
 // jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
 
-// chiedi all utente un numero
-let userNumber = parseInt(prompt('Inserisci un numero'));
-// console.log(userNumber);
+// // chiedi all utente un numero
+// let userNumber = parseInt(prompt('Inserisci un numero'));
+// // console.log(userNumber);
 
-//visualizza i numeri che precedono l'userNumber e fai il cubo
-for (let i = userNumber; i > 0; i--) {  
-    let power = i**userNumber;
-    console.log(i,`elevato alla ${userNumber}=`, power);
+// //visualizza i numeri che precedono l'userNumber e fai il cubo
+// for (let i = userNumber; i > 0; i--) {  
+//     let power = i**3;
+//     console.log(i,`elevato al cubo=`, power);
+// }
+
+
+// -----
+
+let userNumber;
+
+do {
+    userNumber = parseInt(prompt('inserisci un numero'));
+} while (userNumber < 1 || isNaN(userNumber))
+
+
+let counter = 1;
+while (counter <= userNumber) {
+    //calcola cubo
+    const thisCube = counter ** 3
+    console.log(thisCube);
+    counter++;
 }
-
-
-
